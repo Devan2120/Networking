c3a14e5ac69d71b0189b838c52aaebbb186fc8ed
Devan2120 <devananthv2@gmail.com>
2024-08-03 14:37:55 +0530

    upload

---
+++ IOT/Temp_Monitor/Code/conf.py
@@ -0,0 +1,7 @@
+bolt_api_key = "enter bolt api key"
+device_id = "enter device id"
+telegram_chat_id = "enter telegram chat id"
+telegram_bot_id = "enter telegram bot id"
+threshold = 45 * 10.24
+FRAME_SIZE = 10
+MUL_FACTOR = 2
---
+++ IOT/Temp_Monitor/Code/device_restart.py
@@ -0,0 +1,8 @@
+from boltiot import Bolt
+import conf 
+
+api_key = conf.bolt_api_key
+device_id = conf.device_id
+mybolt = Bolt(api_key, device_id)
+response = mybolt.restart()
+print(response)
